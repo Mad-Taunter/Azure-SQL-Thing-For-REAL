50b78221755c3812666cd95e7593f85cdf19eff4
Cairns_Christyan <Cairns_Christyan@edu.mcctc.net>
2020-02-27 13:46:42 -0500

    Add project files.

---
+++ People/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+using People.Models;
+using System;
+using Xamarin.Forms;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace People
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(true)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public async void OnNewButtonClicked(object sender, EventArgs args)
+        {
+            statusMessage.Text = "";
+
+            await App.PersonRepo.AddNewPersonAsync(newPerson.Text);
+            statusMessage.Text = App.PersonRepo.StatusMessage;
+        }
+
+        public async void OnGetButtonClicked(object sender, EventArgs args)
+        {
+            statusMessage.Text = "";
+
+            List<Person> people = await App.PersonRepo.GetAllPeopleAsync();
+            peopleList.ItemsSource = people;
+        }
+
+        public async void DeleteButtonClicked(object sender, EventArgs args)
+        {
+            await App.PersonRepo.DeleteAllPersonAsync(newPerson.Text);
+        }
+    }
+}
