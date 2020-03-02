5928364d34b0ff54277b95efdc4afe223f8060ad
wittingtonfiggletonyerboi@yahoo.com <wittingtonfiggletonyerboi@yahoo.com>
2020-03-02 14:09:40 -0500

    trying to get blob storage to hold the database file

---
+++ People/AzureUpdate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Azure.Storage;
+using Microsoft.Azure.Storage.Blob;
+
+namespace People
+{
+
+    static CloudBlobContainer GetContainer(ContainerType containerType)
+    {
+        var account = CloudStorageAccount.Parse(Constants.StorageConnection);
+        var client = account.CreateCloudBlobClient();
+        return client.GetContainerReference(containerType.ToString().ToLower());
+    }
+
+    class AzureUpdate
+    {
+
+    }
+}
--- People/PersonRepository.cs
+++ People/PersonRepository.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using People.Models;
 using SQLite;
+using Newtonsoft.Json;
 
 namespace People
 {
